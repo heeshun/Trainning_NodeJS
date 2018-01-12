016e6aab0529f4df727b5f0e4690bd27916e6c1d
Nhat Minh <zoungkim@gmail.com>
2018-01-12 16:55:04 +0700

    Task 5:  Search ở màn hình Home

--- public/javascripts/film/list.js
+++ public/javascripts/film/list.js
@@ -1,9 +1,37 @@
 var app = angular.module('appCinema', []).controller('listCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.listFilm = [];
+  $scope.listFilmStable = [];
+  $scope.listTypeFilms = [
+    'Hành động',
+    'Tình cảm',
+    'Khoa học viễn tưởng',
+    'Hài',
+    'Hoạt hình',
+    'Võ thuật',
+    'Kinh dị'
+  ];
 
-  var listFilm = [];
+  $scope.clickSearchType = function (type) {
+    $scope.searchFilm = type;
+  };
 
-  $http.get('/api/cinemas', listFilm).then(function (res) {
-    console.log(res);
+  $http.get('/api/cinemas').then(function (res) {
     $scope.listFilm = res.data;
+    $scope.listFilmStable = res.data;
+  });
+
+  $scope.$watch('searchFilm', function (val) {
+    $scope.listFilm = [];
+    var list = $scope.listFilmStable.filter(x => {
+      if (x.name.includes($scope.searchFilm) || x.author.includes($scope.searchFilm) || x.typeFilm.includes($scope.searchFilm)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    var i;
+    for (i = 0; i < list.length; i++) {
+      $scope.listFilm.push(list[i]);
+    }
   });
 }]);
