58d52bbad2494d205ff4e9e02c410521318b20e5
Nhat Minh <zoungkim@gmail.com>
2018-01-15 17:12:45 +0700

    Fix search

--- public/javascripts/film/list.js
+++ public/javascripts/film/list.js
@@ -125,10 +125,11 @@ var app = angular.module('appCinema', []).controller('listCtrl', ['$scope', '$ht
     $scope.listFilmStable = res.data;
   });
 
+
   $scope.$watch('searchFilm', function (val) {
     $scope.listFilm = [];
     var list = $scope.listFilmStable.filter(x => {
-      if (x.name.includes($scope.searchFilm) || x.author.includes($scope.searchFilm) || x.typeFilm.includes($scope.searchFilm)) {
+      if (changeAlias(x.name).includes(changeAlias($scope.searchFilm)) || changeAlias(x.author).includes(changeAlias($scope.searchFilm)) || changeAlias(x.typeFilm).includes(changeAlias($scope.searchFilm))) {
         return true;
       } else {
         return false;
@@ -140,3 +141,20 @@ var app = angular.module('appCinema', []).controller('listCtrl', ['$scope', '$ht
     }
   });
 }]);
+
+function changeAlias(alias) {
+  var str = alias;
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+  str = str.replace(/đ/g, 'd');
+  str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, ' ');
+  str = str.replace(/ + /g, ' ');
+  str = str.trim();
+  console.log(str);
+  return str;
+}
